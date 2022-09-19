cdac1d921401c3f21837cf7617d52c0038c7eed4
John <vannok@live.com>
2022-09-20 00:26:01 +0300

    commit

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,57 @@
+import { SyntheticEvent, useState } from "react";
+import SingleInput from "./SingleInput";
+
+const arrayWithKeys = [1, 2, 3, 4, 5];
+export default function Inputs() {
+  const [showInputValue, setShowInputValue] = useState("");
+  const [multipleInputsValue, setMultipleInputsValue] = useState<
+    { input: string; key: number }[]
+  >([]);
+  const [showMultipleInputsValue, setShowMultipleInputsValue] = useState<
+    { input: string; key: number }[]
+  >([]);
+  const [resetInputFields, setResetInputFields] = useState(false);
+
+  function handleSendFact(e: SyntheticEvent, fact: string, id: number) {
+    e.preventDefault();
+    const exists = multipleInputsValue.find((v) => v.key === id);
+    if (exists) {
+      const filteredArray = multipleInputsValue.filter((f) => f.key !== id);
+      setMultipleInputsValue([...filteredArray]);
+    }
+    setShowInputValue(fact);
+  }
+
+  function handleSendFactMultiple(e: SyntheticEvent) {
+    e.preventDefault();
+    setShowMultipleInputsValue(multipleInputsValue);
+    setResetInputFields((prev) => !prev);
+    setMultipleInputsValue([]);
+  }
+
+  return (
+    <div>
+      {multipleInputsValue.length > 1 && (
+        <button onClick={handleSendFactMultiple}>
+          Send({multipleInputsValue.length})
+        </button>
+      )}
+      {arrayWithKeys.map((v) => (
+        <SingleInput
+          key={v}
+          handleSendFact={handleSendFact}
+          setMultipleInputsValue={setMultipleInputsValue}
+          multipleInputsValue={multipleInputsValue}
+          resetInputFields={resetInputFields}
+          setResetInputFields={setResetInputFields}
+          uniqueId={v}
+        />
+      ))}
+      <p>Single Input Value {showInputValue}</p>
+      <p>Multiple Inputs Value </p>
+      {showMultipleInputsValue.map((v) => (
+        <p key={v.input}>{v.input}</p>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/SingleInput.tsx
@@ -0,0 +1,60 @@
+import React, { SyntheticEvent, useEffect, useState } from "react";
+
+interface singleInputProps {
+  handleSendFact: (e: SyntheticEvent, fact: string, id: number) => void;
+  setMultipleInputsValue: React.Dispatch<
+    React.SetStateAction<{ input: string; key: number }[]>
+  >;
+  multipleInputsValue: {
+    input: string;
+    key: number;
+  }[];
+  uniqueId: number;
+  setResetInputFields: React.Dispatch<React.SetStateAction<boolean>>;
+  resetInputFields: boolean;
+}
+
+export default function SingleInput(p: singleInputProps) {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (p.resetInputFields) {
+      setInputValue("");
+    }
+    p.setResetInputFields(false);
+  }, [p.resetInputFields]);
+
+  function handleChangeInputValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+    const exists = p.multipleInputsValue.find((v) => v.key === p.uniqueId);
+    if (exists) {
+      const filteredArray = p.multipleInputsValue.filter(
+        (f) => f.key !== p.uniqueId,
+      );
+      p.setMultipleInputsValue([
+        ...filteredArray,
+        { input: e.target.value, key: p.uniqueId },
+      ]);
+    } else {
+      p.setMultipleInputsValue((prev) => [
+        ...prev,
+        { input: e.target.value, key: p.uniqueId },
+      ]);
+    }
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        p.handleSendFact(e, inputValue, p.uniqueId);
+        setInputValue("");
+      }}>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleChangeInputValue}
+      />
+      <button style={{ display: "none" }}></button>
+    </form>
+  );
+}
